298367946c925c8eb7df9b078061cae73928b3ae
AndreTati <andrea.duran@alumnos.frm.utn.edu.ar>
2025-06-23 20:32:37 -0300

    correcciones

--- prod/app.py
+++ prod/app.py
@@ -5,13 +5,14 @@ import plotly.express as px
 from utils import load_model, get_bert_embeddings, get_verb_embedding, descripcion_tiempos, descripcion_modos, descripcion_personas, descripcion_numeros
 
 # Cambiar ancho de página
-st.set_page_config(layout="wide")
+description = "Predicción de Tiempos Verbales"
+st.set_page_config(page_title=description, layout="wide")
 
 # Cargar modelos
 modelTime, modelPerson, modelNumber, id2tense, id2person, id2number, tokenizer, bert_model, nlp = load_model()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-st.title("Predicción de Tiempos Verbales")
+st.title(description)
 
 # Mantener estado
 if "oracion" not in st.session_state:
@@ -95,6 +96,23 @@ if st.session_state.seleccionado:
         st.write(f"• Persona: {descripcion_personas.get(int(pred_persona), pred_persona)}")
         st.write(f"• Número: {descripcion_numeros.get(pred_numero, pred_numero)}")
 
-        st.plotly_chart(px.bar(x=labels_tm, y=probs_tm, text_auto=True, title="Tiempo - Modo", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
-        st.plotly_chart(px.bar(x=labels_p, y=probs_p, text_auto=True, title="Persona", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
-        st.plotly_chart(px.bar(x=labels_n, y=probs_n, text_auto=True, title="Número", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
+        #st.plotly_chart(px.bar(x=labels_tm, y=probs_tm, text_auto=True, title="Tiempo - Modo", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
+        #st.plotly_chart(px.bar(x=labels_p, y=probs_p, text_auto=True, title="Persona", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
+        #st.plotly_chart(px.bar(x=labels_n, y=probs_n, text_auto=True, title="Número", labels={"x": "Etiqueta", "y": "Probabilidad"}), width=300, use_container_width=False)
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.plotly_chart(
+                px.bar(x=labels_tm, y=probs_tm, text_auto=True, title="Tiempo - Modo", labels={"x": "Etiqueta", "y": "Probabilidad"}, text_auto=True),
+                use_container_width=True
+            )
+        with col2:
+            st.plotly_chart(
+                px.bar(x=labels_p, y=probs_p, text_auto=True, title="Persona", labels={"x": "Etiqueta", "y": "Probabilidad"}),
+                use_container_width=True
+            )
+        with col3:
+            st.plotly_chart(
+                px.bar(x=labels_n, y=probs_n, text_auto=True, title="Número", labels={"x": "Etiqueta", "y": "Probabilidad"}),
+                use_container_width=True
+            )
